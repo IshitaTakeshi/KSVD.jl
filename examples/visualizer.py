1161adfa9488b9c64a7a96e7a990147a25c54e42
IshitaTakeshi <ishitah.takeshi@gmail.com>
2016-09-26 23:47:40 +0900

    Swap image filenames

--- examples/visualizer.py
+++ examples/visualizer.py
@@ -14,13 +14,13 @@ for i, image in enumerate(digits.images[:256]):
     ax = fig1.add_subplot(16, 16, i+1)
     ax.imshow(image, cmap='Greys_r')
     ax.set_axis_off()
-fig1.savefig("digits256.png", bbox_inches="tight")
+fig1.savefig("digit_images.png", bbox_inches="tight")
 
 fig2.suptitle("Obtained dictionary", fontsize=18)
 for i, atom in enumerate(D):
     ax = fig2.add_subplot(16, 16, i+1)
     ax.imshow(atom.reshape(8, 8), cmap='Greys_r')
     ax.set_axis_off()
-fig2.savefig("digit_images.png", bbox_inches="tight")
+fig2.savefig("digits256.png", bbox_inches="tight")
 
 plt.show()
